6acc26fa4fda1577be9b90c41840131f574a87ac
Sagar-56 <sb0473564@gmail.com>
2022-04-14 11:55:53 +0530

    code change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongo = require('mongodb');
 const MongoClient = mongo.MongoClient;
 const dotenv = require('dotenv')
 dotenv.config()
-let port = 4356;
+let port = process.env.PORT || 4356;
 const mongoUrl = "mongodb://localhost:27017";
 const mongoLiveUrl = "mongodb+srv://Sagarbehera:Sagar456@cluster0.96hmj.mongodb.net/eduInternJan?retryWrites=true&w=majority";
 const bodyParser = require('body-parser');
